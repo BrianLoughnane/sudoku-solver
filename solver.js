7c638811b37e4ee72f0b6be5f35771601f836f1f
BrianLoughnane <BrianJLoughnane@gmail.com>
2015-06-22 19:03:38 -0700

    changed console logs and added another board

--- solver.js
+++ solver.js
@@ -127,6 +127,8 @@ $(document).on('ready', function () {
       var removeValue = function(rowIndex, colIndex) {
         matrix[rowIndex][colIndex].value = 0;
       }
+
+      //helpers
       var mapValues = function () {
         return matrix.map(function (row) { 
           return row.map(function (cell) { 
@@ -134,9 +136,17 @@ $(document).on('ready', function () {
           });
         });
       }
+
+
       console.log('mapValues right before declaration', mapValues)
 
       var inner = function (rowIndex, colIndex, value, reverse) {
+        // helper
+        var rcv = function (r, c, v) {
+          return _.isEqual([rowIndex, colIndex, value], [r,c,v]);
+        }
+        // end helper
+
         console.log('mapValues', mapValues)
         console.log('INNER:', '(',rowIndex,',', colIndex,')', 'value', value);
 
@@ -173,7 +183,7 @@ $(document).on('ready', function () {
                 return nextColWorks; // return the solution
               } else { // otherwise
                 console.log('currently at:', '(',rowIndex,',', colIndex,')', 'value', value);
-                console.log('nextCol didnt work with current spread:', '(',rowIndex,',', colIndex+1,')');
+                console.log('nextCol didnt work with current spread at:', '(',rowIndex,',', colIndex,')');
                 if(value < 9) { // if the value is less than 9...
                   var initValue = value;
                   var nextValWorks = inner(rowIndex, colIndex, ++value); // remain in this column, increment value, and check for a solution
@@ -181,8 +191,10 @@ $(document).on('ready', function () {
                     return nextValWorks; // return it
                   } else { // otherwise
                     console.log('currently at:', '(',rowIndex,',', colIndex,')', 'value', value);
-                    console.log('nextVal didnt work:', '(',rowIndex,',', colIndex,')', 'value', value+1, 'reverse', true);
+                    console.log('nextVal didnt work:', '(',rowIndex,',', colIndex,')', 'value', value, 'reverse', true);
+                    // if(rcv(0,1,5)) { debugger; } 
                     return false
+                    // return inner(rowIndex, colIndex, ++value);
                   }
                 } else { // if the value is not less than 9...
                   return false; // end recursion
@@ -239,7 +251,512 @@ $(document).on('ready', function () {
 
 
 
-
+// [
+//   [
+//     {
+//       "rowIndex": 0,
+//       "cellIndex": 0,
+//       "set": true,
+//       "value": "7"
+//     },
+//     {
+//       "rowIndex": 0,
+//       "cellIndex": 1,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 0,
+//       "cellIndex": 2,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 0,
+//       "cellIndex": 3,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 0,
+//       "cellIndex": 4,
+//       "set": true,
+//       "value": "2"
+//     },
+//     {
+//       "rowIndex": 0,
+//       "cellIndex": 5,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 0,
+//       "cellIndex": 6,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 0,
+//       "cellIndex": 7,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 0,
+//       "cellIndex": 8,
+//       "set": true,
+//       "value": "4"
+//     }
+//   ],
+//   [
+//     {
+//       "rowIndex": 1,
+//       "cellIndex": 0,
+//       "set": true,
+//       "value": "8"
+//     },
+//     {
+//       "rowIndex": 1,
+//       "cellIndex": 1,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 1,
+//       "cellIndex": 2,
+//       "set": true,
+//       "value": "3"
+//     },
+//     {
+//       "rowIndex": 1,
+//       "cellIndex": 3,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 1,
+//       "cellIndex": 4,
+//       "set": true,
+//       "value": "4"
+//     },
+//     {
+//       "rowIndex": 1,
+//       "cellIndex": 5,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 1,
+//       "cellIndex": 6,
+//       "set": true,
+//       "value": "9"
+//     },
+//     {
+//       "rowIndex": 1,
+//       "cellIndex": 7,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 1,
+//       "cellIndex": 8,
+//       "set": true,
+//       "value": "7"
+//     }
+//   ],
+//   [
+//     {
+//       "rowIndex": 2,
+//       "cellIndex": 0,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 2,
+//       "cellIndex": 1,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 2,
+//       "cellIndex": 2,
+//       "set": true,
+//       "value": "2"
+//     },
+//     {
+//       "rowIndex": 2,
+//       "cellIndex": 3,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 2,
+//       "cellIndex": 4,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 2,
+//       "cellIndex": 5,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 2,
+//       "cellIndex": 6,
+//       "set": true,
+//       "value": "3"
+//     },
+//     {
+//       "rowIndex": 2,
+//       "cellIndex": 7,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 2,
+//       "cellIndex": 8,
+//       "set": false,
+//       "value": ""
+//     }
+//   ],
+//   [
+//     {
+//       "rowIndex": 3,
+//       "cellIndex": 0,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 3,
+//       "cellIndex": 1,
+//       "set": true,
+//       "value": "7"
+//     },
+//     {
+//       "rowIndex": 3,
+//       "cellIndex": 2,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 3,
+//       "cellIndex": 3,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 3,
+//       "cellIndex": 4,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 3,
+//       "cellIndex": 5,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 3,
+//       "cellIndex": 6,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 3,
+//       "cellIndex": 7,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 3,
+//       "cellIndex": 8,
+//       "set": false,
+//       "value": ""
+//     }
+//   ],
+//   [
+//     {
+//       "rowIndex": 4,
+//       "cellIndex": 0,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 4,
+//       "cellIndex": 1,
+//       "set": true,
+//       "value": "6"
+//     },
+//     {
+//       "rowIndex": 4,
+//       "cellIndex": 2,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 4,
+//       "cellIndex": 3,
+//       "set": true,
+//       "value": "9"
+//     },
+//     {
+//       "rowIndex": 4,
+//       "cellIndex": 4,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 4,
+//       "cellIndex": 5,
+//       "set": true,
+//       "value": "5"
+//     },
+//     {
+//       "rowIndex": 4,
+//       "cellIndex": 6,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 4,
+//       "cellIndex": 7,
+//       "set": true,
+//       "value": "1"
+//     },
+//     {
+//       "rowIndex": 4,
+//       "cellIndex": 8,
+//       "set": false,
+//       "value": ""
+//     }
+//   ],
+//   [
+//     {
+//       "rowIndex": 5,
+//       "cellIndex": 0,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 5,
+//       "cellIndex": 1,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 5,
+//       "cellIndex": 2,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 5,
+//       "cellIndex": 3,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 5,
+//       "cellIndex": 4,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 5,
+//       "cellIndex": 5,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 5,
+//       "cellIndex": 6,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 5,
+//       "cellIndex": 7,
+//       "set": true,
+//       "value": "8"
+//     },
+//     {
+//       "rowIndex": 5,
+//       "cellIndex": 8,
+//       "set": false,
+//       "value": ""
+//     }
+//   ],
+//   [
+//     {
+//       "rowIndex": 6,
+//       "cellIndex": 0,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 6,
+//       "cellIndex": 1,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 6,
+//       "cellIndex": 2,
+//       "set": true,
+//       "value": "9"
+//     },
+//     {
+//       "rowIndex": 6,
+//       "cellIndex": 3,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 6,
+//       "cellIndex": 4,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 6,
+//       "cellIndex": 5,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 6,
+//       "cellIndex": 6,
+//       "set": true,
+//       "value": "6"
+//     },
+//     {
+//       "rowIndex": 6,
+//       "cellIndex": 7,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 6,
+//       "cellIndex": 8,
+//       "set": false,
+//       "value": ""
+//     }
+//   ],
+//   [
+//     {
+//       "rowIndex": 7,
+//       "cellIndex": 0,
+//       "set": true,
+//       "value": "4"
+//     },
+//     {
+//       "rowIndex": 7,
+//       "cellIndex": 1,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 7,
+//       "cellIndex": 2,
+//       "set": true,
+//       "value": "6"
+//     },
+//     {
+//       "rowIndex": 7,
+//       "cellIndex": 3,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 7,
+//       "cellIndex": 4,
+//       "set": true,
+//       "value": "5"
+//     },
+//     {
+//       "rowIndex": 7,
+//       "cellIndex": 5,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 7,
+//       "cellIndex": 6,
+//       "set": true,
+//       "value": "8"
+//     },
+//     {
+//       "rowIndex": 7,
+//       "cellIndex": 7,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 7,
+//       "cellIndex": 8,
+//       "set": true,
+//       "value": "1"
+//     }
+//   ],
+//   [
+//     {
+//       "rowIndex": 8,
+//       "cellIndex": 0,
+//       "set": true,
+//       "value": "5"
+//     },
+//     {
+//       "rowIndex": 8,
+//       "cellIndex": 1,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 8,
+//       "cellIndex": 2,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 8,
+//       "cellIndex": 3,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 8,
+//       "cellIndex": 4,
+//       "set": true,
+//       "value": "3"
+//     },
+//     {
+//       "rowIndex": 8,
+//       "cellIndex": 5,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 8,
+//       "cellIndex": 6,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 8,
+//       "cellIndex": 7,
+//       "set": false,
+//       "value": ""
+//     },
+//     {
+//       "rowIndex": 8,
+//       "cellIndex": 8,
+//       "set": true,
+//       "value": "2"
+//     }
+//   ]
+// ]
 
 
 
